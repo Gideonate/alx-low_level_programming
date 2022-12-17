7e6c197e917462339830b1bade0f9cd83197237f
Gideonites <gideonsaved@gmail.com>
2022-12-17 15:45:39 +0000

    alph

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alph[24] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		putchar(alph[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
