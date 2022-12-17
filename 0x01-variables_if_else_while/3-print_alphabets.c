c11c853082618a631e655f817d4421bf993d314a
Gideonites <gideonsaved@gmail.com>
2022-12-17 15:34:49 +0000

    alph

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in order of aA
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char alph[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alph[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
